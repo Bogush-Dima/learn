af9d8ad47f1c9ee9bbad7326d7d25d2023c7a20e
Bogush-Dima <bogusd66@gmail.com>
2022-02-11 19:09:22 +0300

    added connect wallet logic

--- metamask/src/App.js
+++ metamask/src/App.js
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import detectEthereumProvider from '@metamask/detect-provider';
 
 const provider = await detectEthereumProvider();
 
 if (provider) {
-  console.log(provider);
+  console.log("Provider:", provider);
+  console.log("Is Connected:", provider.isConnected());
 } else {
   console.log('Please install MetaMask!');
 }
 
 export const App = () => {
+  const [currentChainId, setCurrentChainId] = useState(provider.isConnected() ? provider.selectedAddress : null)
+
+  const connectWallet = () => {
+    provider
+      .request({ method: "eth_requestAccounts" })
+      .then(result => {
+        console.log("Connect Result:", result)
+        setCurrentChainId(result)
+      })
+      .catch(error => {
+        console.log("Connect Error:", error)
+      })
+  }
+
   return (
     <div className="App">
+        <button onClick={connectWallet} disabled={currentChainId}>Connect</button>
     </div>
   );
 }
